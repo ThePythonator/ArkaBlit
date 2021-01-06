526c38f8e2df01b295d726d1155ada11124c8151
ThePythonator <49040244+ThePythonator@users.noreply.github.com>
2021-01-06 12:02:58 +0000

    Basic game mechanics finished.

--- game.cpp
+++ game.cpp
@@ -7,45 +7,64 @@
 #define SPRITE_SIZE 8
 #define BORDER 2
 
-#define PADDLE_SPEED 100
+#define MAX_X_VELOCITY 0.95
+
+#define PADDLE_SPEED 55
+#define BALL_SPEED 70
+
+#define MIN_JOYSTICK 0.2
 
 #define DEFAULT_WIDTH 10
 #define DEFAULT_HEALTH 3
 
-#define LEVEL_COUNT 1
+#define LEVEL_COUNT 2
 #define LEVEL_WIDTH 10
 #define LEVEL_HEIGHT 8
+
+#define BLOCK_VALUE 5
 
 using namespace blit;
 
 struct Paddle {
-    float xPosition;
+    float xPosition, yPosition;
 
     int width;
 
     int health;
     int score;
+
+    int combo; // used for combos
 };
 
 struct Block {
     int xPosition, yPosition;
 
+    int row, column;
+
     int health;
+
+    bool noValue;
 };
 
 struct Ball {
     float xPosition, yPosition;
     float xVelocity, yVelocity;
+
+    bool held;
 };
 
 void render_blocks();
 void render_block(Block);
 void render_player();
 void render_hud();
+void render_ball();
 void start_game();
 void start_level(int);
+void reset_ball();
 void load_level(int levelLayout[LEVEL_HEIGHT][LEVEL_WIDTH]);
 Block generate_block(int, int, int);
+void handle_block_collisions();
+int blocks_remaining();
 
 int state = 0;
 float dt;
@@ -87,9 +106,47 @@ int levelLayouts[LEVEL_COUNT][LEVEL_HEIGHT][LEVEL_WIDTH] = {
         },
         {
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        }
+    },
+    {
+        {
+            0, 0, 0, 1, 1, 1, 1, 0, 0, 0
         },
+        {
+            0, 0, 1, 1, 2, 2, 1, 1, 0, 0
+        },
+        {
+            0, 1, 1, 2, 3, 3, 2, 1, 1, 0
+        },
+        {
+            0, 0, 1, 1, 2, 2, 1, 1, 0, 0
+        },
+        {
+           -2,-2,-2, 1, 1, 1, 1,-2,-2,-2
+        },
+        {
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        },
+        {
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        },
+        {
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        }
     }
 };
+
+float min(float a, float b) {
+    return a < b ? a : b;
+}
+
+float max(float a, float b) {
+    return a > b ? a : b;
+}
+
+float clamp(float x, float mi, float ma) {
+    return min(max(x, mi), ma);
+}
 
 void render_blocks() {
     for (int i = 0; i < blocks.size(); i++) {
@@ -98,21 +155,29 @@ void render_blocks() {
 }
 
 void render_block(Block block) {
-    screen.sprite((block.health - 1) * 2, Point(block.xPosition, block.yPosition));
-    screen.sprite((block.health - 1) * 2 + 1, Point(block.xPosition + SPRITE_SIZE, block.yPosition));
+    int index = block.health - 1;
+    if (block.noValue) {
+        index = 10 - block.health;
+    }
+    if (block.health == -1) {
+        index = 6;
+    }
+    if (block.health != 0) {
+        screen.sprite(index * 2, Point(block.xPosition, block.yPosition));
+        screen.sprite(index * 2 + 1, Point(block.xPosition + SPRITE_SIZE, block.yPosition));
+    }
 }
 
 void render_player() {
     int left = player.xPosition - player.width; // fix offset?
-    int yPosition = SCREEN_HEIGHT - BORDER * 2;
 
-    screen.blit(screen.sprites, Rect(4, 16, 1, 4), Point(left, yPosition));
+    screen.blit(screen.sprites, Rect(4, 16, 1, 4), Point(left, player.yPosition));
 
     for (int i = 0; i < player.width - 1; i++) {
-        screen.blit(screen.sprites, Rect(5, 16, 2, 4), Point(left + 1 + i * 2, yPosition));
+        screen.blit(screen.sprites, Rect(5, 16, 2, 4), Point(left + 1 + i * 2, player.yPosition));
     }
 
-    screen.blit(screen.sprites, Rect(7, 16, 1, 4), Point(left + player.width * 2 - 1, yPosition));
+    screen.blit(screen.sprites, Rect(7, 16, 1, 4), Point(left + player.width * 2 - 1, player.yPosition));
 }
 
 void render_hud() {
@@ -155,8 +220,19 @@ void render_hud() {
     // :
     screen.blit(screen.sprites, Rect(72, 24, 2, 8), Point(SCREEN_WIDTH / 2 + 25, BORDER));
 
-    // <levelnumber
+    // <levelnumber>
     screen.blit(screen.sprites, Rect(4 * (levelNumber + 1), 24, 4, 8), Point(SCREEN_WIDTH / 2 + 28, BORDER));
+
+    int multiplier = 1 + player.combo / 2;
+
+    if (multiplier > 1) {
+        // multiplier
+        screen.text("x" + std::to_string(player.combo), minimal_font, Point(BORDER * 4, BORDER * 8), true, TextAlign::center_center);
+    }
+}
+
+void render_ball() {
+    screen.blit(screen.sprites, Rect(0, 16, 4, 4), Point(ball.xPosition, ball.yPosition));
 }
 
 void start_game() {
@@ -166,11 +242,26 @@ void start_game() {
     start_level(0);
 }
 
-void start_level(int levelNumber) {
+void start_level(int levelNum) {
+    levelNumber = levelNum;
     player.xPosition = SCREEN_WIDTH / 2;
     player.width = DEFAULT_WIDTH;
 
     load_level(levelLayouts[levelNumber]);
+
+    reset_ball();
+}
+
+void reset_ball() {
+    ball.xPosition = player.xPosition - SPRITE_SIZE / 4;
+    ball.yPosition = player.yPosition - SPRITE_SIZE / 2;
+
+    ball.xVelocity = 0;
+    ball.yVelocity = 0;
+
+    ball.held = true;
+
+    player.combo = 0;
 }
 
 void load_level(int levelLayout[LEVEL_HEIGHT][LEVEL_WIDTH]) {
@@ -178,7 +269,7 @@ void load_level(int levelLayout[LEVEL_HEIGHT][LEVEL_WIDTH]) {
 
     for (int y = 0; y < LEVEL_HEIGHT; y++) {
         for (int x = 0; x < LEVEL_WIDTH; x++) {
-            if (levelLayout[y][x] > 0) {
+            if (levelLayout[y][x] != 0) {
                 blocks.push_back(generate_block(levelLayout[y][x], x, y));
             }
         }
@@ -190,7 +281,83 @@ Block generate_block(int health, int x, int y) {
     block.health = health;
     block.xPosition = x * SPRITE_SIZE * 2;
     block.yPosition = (y + 1.5) * SPRITE_SIZE;
+
+    if (health == -2) {
+        block.health = 3;
+        block.noValue = true;
+    }
+    else {
+        block.noValue = false;
+    }
+
+    block.row = y;
+    block.column = x;
+
     return block;
+}
+
+void handle_block_collisions() {
+    // check this row and the next
+    int row = (ball.yPosition - SPRITE_SIZE * 2) / (float)SPRITE_SIZE;
+
+    // check this column and the next
+    int column = (ball.xPosition - SPRITE_SIZE * 0.5) / (float)(SPRITE_SIZE * 2);
+
+    //printf("%d,%d\n",row, column);
+
+    for (int i = 0; i < blocks.size(); i++) {
+        if (blocks.at(i).health != 0) {
+            if (blocks.at(i).row == row || blocks.at(i).row == row + 1) {
+                if (blocks.at(i).column == column || blocks.at(i).column == column + 1) {
+                    // probably collided with block, now check the side the ball collided with
+                    if (ball.xPosition + SPRITE_SIZE / 2 > blocks.at(i).xPosition && ball.xPosition < blocks.at(i).xPosition + SPRITE_SIZE * 2) {
+                        if (ball.yPosition + SPRITE_SIZE / 2 > blocks.at(i).yPosition && ball.yPosition < blocks.at(i).yPosition + SPRITE_SIZE) {
+                            // definitely collided, take 1 hp off the block
+                            blocks.at(i).health -= 1;
+
+                            // now we need to check which side the ball collided with in order handle rebounds
+                            if (ball.yVelocity < 0 && ball.yPosition > blocks.at(i).yPosition + SPRITE_SIZE / 2) {
+                                // hit on bottom
+                                ball.yVelocity = std::abs(ball.yVelocity);
+                            }
+                            else if (ball.yVelocity > 0 && ball.yPosition + SPRITE_SIZE / 2 < blocks.at(i).yPosition + SPRITE_SIZE / 2) {
+                                // hit on top
+                                ball.yVelocity = -std::abs(ball.yVelocity);
+                            }
+                            else if (ball.xVelocity > 0 && ball.xPosition + SPRITE_SIZE / 2 < blocks.at(i).xPosition + SPRITE_SIZE) {
+                                // hit on left
+                                ball.xVelocity = -std::abs(ball.xVelocity);
+                            }
+                            else if (ball.xVelocity < 0 && ball.xPosition > blocks.at(i).xPosition + SPRITE_SIZE) {
+                                // hit on right
+                                ball.xVelocity = std::abs(ball.xVelocity);
+                            }
+
+                            if (!blocks.at(i).noValue && blocks.at(i).health >= 0) {
+                                // need to add points, block wasn't a wall
+
+                                // calculate multiplier, increase score by adjusted value
+                                player.score += BLOCK_VALUE * (int)(1 + player.combo / 2);
+
+                                // increase combo after adjusting score
+                                player.combo += 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+int blocks_remaining() {
+    int total = 0;
+    for (int i = 0; i < blocks.size(); i++) {
+        if (blocks.at(i).health > 0 && !blocks.at(i).noValue) {
+            total++;
+        }
+    }
+    return total;
 }
 
 ///////////////////////////////////////////////////////////////////////////
@@ -202,6 +369,8 @@ Block generate_block(int health, int x, int y) {
 void init() {
     set_screen_mode(ScreenMode::lores);
     screen.sprites = SpriteSheet::load(asset_sprites);
+
+    player.yPosition = SCREEN_HEIGHT - BORDER * 2;
 }
 
 ///////////////////////////////////////////////////////////////////////////
@@ -224,7 +393,7 @@ void render(uint32_t time) {
     screen.blit(background, Rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT), Point(0, 0), false);
 
     if (state == 0) {
-        screen.text("Jumpy Squirrel", minimal_font, Point(SCREEN_WIDTH / 2, SCREEN_HEIGHT * 1 / 3), true, TextAlign::center_center); // change to custom icon
+        screen.text("ArkaBlit", minimal_font, Point(SCREEN_WIDTH / 2, SCREEN_HEIGHT * 1 / 3), true, TextAlign::center_center); // change to custom icon
 
         screen.text("Press A to Start", minimal_font, Point(SCREEN_WIDTH / 2, SCREEN_HEIGHT * 2 / 3), true, TextAlign::center_center);
     }
@@ -232,6 +401,8 @@ void render(uint32_t time) {
         render_blocks();
 
         render_player();
+
+        render_ball();
 
         render_hud();
     }
@@ -257,11 +428,66 @@ void update(uint32_t time) {
         }
     }
     else if (state == 1) {
-        if (buttons.pressed & Button::DPAD_LEFT) {
-
+        if ((buttons & Button::DPAD_LEFT) || joystick.x < -MIN_JOYSTICK) {
+            player.xPosition -= PADDLE_SPEED * dt;
         }
-        else if (buttons.pressed & Button::DPAD_RIGHT) {
+        if ((buttons & Button::DPAD_RIGHT) || joystick.x > MIN_JOYSTICK) {
+            player.xPosition += PADDLE_SPEED * dt;
+        }
 
+        player.xPosition = clamp(player.xPosition, player.width, SCREEN_WIDTH - player.width);
+
+        if (ball.xPosition < 0 || ball.xPosition + SPRITE_SIZE / 2 > SCREEN_WIDTH) {
+            ball.xVelocity = -ball.xVelocity;
+        }
+        if (ball.yPosition < SPRITE_SIZE * 1.5) {
+            ball.yPosition = SPRITE_SIZE * 1.5;
+            ball.yVelocity = -ball.yVelocity;
+        }
+        if (ball.yPosition > SCREEN_HEIGHT + SPRITE_SIZE / 2) {
+            player.health -= 1;
+            reset_ball();
+        }
+
+        ball.xPosition += ball.xVelocity * BALL_SPEED * dt;
+        ball.yPosition += ball.yVelocity * BALL_SPEED * dt;
+
+        if (ball.held) {
+            reset_ball();
+
+            if (buttons.pressed & Button::A) {
+                ball.held = false;
+                ball.yVelocity = -1;
+            }
+        }
+
+        //handle ball collisions
+
+        // Paddle collision
+        if (ball.xPosition > player.xPosition - player.width - SPRITE_SIZE / 4 && ball.xPosition + SPRITE_SIZE / 2 < player.xPosition + player.width + SPRITE_SIZE / 4 && ball.yVelocity > 0) {
+            if (ball.yPosition + SPRITE_SIZE / 2 > player.yPosition && ball.yPosition < player.yPosition + SPRITE_SIZE / 2) {
+                //ball.xVelocity = clamp(ball.xVelocity - ((player.xPosition - (ball.xPosition + SPRITE_SIZE / 4)) / (float)(player.width + SPRITE_SIZE / 2)), -MAX_X_VELOCITY, MAX_X_VELOCITY);
+                // new version below ----v----- deflects the ball less
+                ball.xVelocity = clamp(ball.xVelocity - ((player.xPosition - (ball.xPosition + SPRITE_SIZE / 4)) / (float)(player.width * 2)), -MAX_X_VELOCITY, MAX_X_VELOCITY);
+
+                ball.yVelocity = -std::sqrt(1 - (ball.xVelocity * ball.xVelocity));
+
+                // reset player combo
+                player.combo = 0;
+            }
+        }
+
+        handle_block_collisions();
+
+        // level admin stuff
+
+        if (blocks_remaining() == 0) {
+            // next level
+            start_level(levelNumber + 1);
+        }
+
+        if (player.health == 0) {
+            // player died
         }
     }
 }
